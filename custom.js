d8c80963dbaec9ade80131e38de5de815e126e2a
Akshay Kanthi <aksanoble@gmail.com>
2016-05-12 16:37:09 +0530

    Fall back to Mustache.JS

--- custom.js
+++ custom.js
@@ -1,6 +1,5 @@
-$.getJSON('bird.json', function(bird) {
-  var template = $('#personTpl').html();
-  var compiled = Handlebars.compile(template)
-  var html = compiled(bird)
-  $('#sampleArea').html(html);
+jQuery.getJSON('bird.json', function(bird) {
+    var template = jQuery('#personTpl').html();
+    var html = Mustache.to_html(template, bird);
+    jQuery('#sampleArea').html(html);
 });
